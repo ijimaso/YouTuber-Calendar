8793e320a9622698d8cd109c28b5d020c0dc8165
ijimaso <soxup0708@gmail.com>
2019-08-03 21:10:01 +0900

    update

--- src/back/fetch.js
+++ src/back/fetch.js
@@ -1,15 +1,29 @@
 // HTTP通信を行って，データを取得するサンプルコード
-function getUserInfo(userId) {
-    fetch(`https://api.github.com/users/${userId}`)
+const fetchYouTubeData = (channelId, key) => {
+    const header = new Headers();
+    header.append('Access-Control-Allow-Origin', 'text/plain');
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=10&key=${key}`,
+        {
+            method: 'GET',
+            mode: 'cors',
+            header
+        })
         .then(response => {
             console.log(response.status);
             if (!response.ok) {
                 console.error("サーバーエラー", response);
             } else {
-                response.json().then(userInfo => {
-                    const p = document.getElementById('p');
-                    console.log(userInfo['login']);
-                    p.innerHTML = userInfo['login'];
+                response.json().then(results => {
+                    // const p = document.getElementById('p');
+                    const items = results.items;
+                    items.forEach(item => {
+                        const { publishedAt, title } = item.snippet;
+                        const { videoId } = item.id;
+                        const thumbnail = item.snippet.thumbnails.medium.url;
+                        console.log(publishedAt, title, videoId);
+                        console.log(thumbnail);
+                    });
+                    // p.innerHTML = results;
                 })
             }
         }).catch(error => {
--- src/front/index.js
+++ src/front/index.js
@@ -1,23 +1,27 @@
-// カレンダー表示
-document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+document.addEventListener('DOMContentLoaded', () => {
+    const calendarEl = document.getElementById('calendar');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: ['dayGrid'],
         events: [
             {
-            title: 'event1',
-            start: '2019-07-23'
+            title: '【罠の応酬】文理対抗！マリオメーカー対決！',
+            start: '2019-08-03',
+            url: 'https://www.youtube.com/watch?v=JcyHfkY4-3U',
+            imageurl: 'https://i.ytimg.com/vi/B2EZyROtZ20/mqdefault.jpg',
             },
             {
-                title: 'event2',
-                start: '2019-07-30'
-            },
-            {
-                title: 'event3',
-                start: '2019-07-08'
-            },]
-    });
+                title: '【罠の応酬】文理対抗！マリオメーカー対決！',
+                start: '2019-08-05',
+                url: 'https://www.youtube.com/watch?v=JcyHfkY4-3U',
+                imageurl: 'https://i.ytimg.com/vi/B2EZyROtZ20/mqdefault.jpg',
+            }],
+        eventRender: (info) => {
+            if (info.event.extendedProps.imageurl) {
+                info.el.querySelector('.fc-title').innerHTML =
+                    `<img src='${info.event.extendedProps.imageurl}' width='170' height='100'><br>
+                            ${info.event.title}`;
+            }
+        }
+    })
     calendar.render();
 });
-

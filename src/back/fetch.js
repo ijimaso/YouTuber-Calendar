d1c4d8370414dabfbd0c58caee14873b58634be3
ijimaso <soxup0708@gmail.com>
2019-08-06 12:32:46 +0900

    update

--- src/back/fetch.js
+++ src/back/fetch.js
@@ -2,28 +2,39 @@
 const fetchYouTubeData = (channelId, key) => {
     const header = new Headers();
     header.append('Access-Control-Allow-Origin', 'text/plain');
+    // console.log(header);
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=10&key=${key}`,
         {
-            method: 'GET',
             mode: 'cors',
             header
         })
         .then(response => {
-            console.log(response.status);
+            // console.log(response);
+            // console.log(response.status);
+            // console.log(response.text);
             if (!response.ok) {
                 console.error("サーバーエラー", response);
             } else {
                 response.json().then(results => {
-                    // const p = document.getElementById('p');
+                    console.log(results);
                     const items = results.items;
+                    const events = [];
                     items.forEach(item => {
                         const { publishedAt, title } = item.snippet;
                         const { videoId } = item.id;
-                        const thumbnail = item.snippet.thumbnails.medium.url;
-                        console.log(publishedAt, title, videoId);
-                        console.log(thumbnail);
+                        const imageurl = item.snippet.thumbnails.medium.url;
+                        const start = publishedAt.split("T")[0];
+                        const event = {
+                            title,
+                            start,
+                            url: `https://www.youtube.com/watch?v=${videoId}`,
+                            imageurl
+                        };
+                        console.log(event);
+                        events.push(event);
                     });
-                    // p.innerHTML = results;
+                    console.log(calendar);
+                    calendar.addEventSource(events);
                 })
             }
         }).catch(error => {

0de3739c0fad06dee985e4fe72a858bd011a1905
ijimaso <soxup0708@gmail.com>
2019-10-26 17:42:24 +0900

    add functions

--- src/front/calendar.js
+++ src/front/calendar.js
@@ -1,5 +1,47 @@
+// 動画情報を取得する関数
+const crawlVideos = async (calendar, pageToken) => {
+    const channelId = document.getElementById('dropdown-channels').value;
+
+    let crawlVideosUrl;
+    if (!pageToken) {
+        crawlVideosUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=10&key=${apikey['key']}`;
+    } else {
+        crawlVideosUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=10&pageToken=${pageToken}&key=${apikey['key']}`
+    }
+
+    const resultsVideos = await fetch(crawlVideosUrl, {
+        method: "GET",
+        mode: 'cors',
+        headers: { "Content-Type": "Access-Control-Allow-Origin; text/plain" }
+    }).catch((err) => {
+        console.log(err);
+    });
+    const resultsVideosJson = await resultsVideos.json();
+
+    const { items } = resultsVideosJson;
+    items.forEach(item => {
+        const { publishedAt, title, channelTitle } = item.snippet;
+        const { videoId } = item.id;
+        const imageurl = item.snippet.thumbnails.medium.url;
+
+        const event = {
+            title,
+            start: publishedAt.split("T")[0],
+            url: `https://www.youtube.com/watch?v=${videoId}`,
+            imageurl,
+            channelTitle
+        };
+        calendar.addEvent(event);
+    });
+
+    // nextPageTokenを取得
+    const { nextPageToken } = resultsVideosJson;
+    pagingToken = nextPageToken;
+};
+
 let pagingToken;
 
+// カレンダーUIを描画する
 document.addEventListener('DOMContentLoaded', () => {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
@@ -20,75 +62,13 @@ document.addEventListener('DOMContentLoaded', () => {
             crawlButton: {
                 text: '取得',
                 click: async () => {
-                    const channelId = document.getElementById('dropdown-channels').value;
-
-                    // 動画情報を取得
-                    const crawlVideosUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=10&key=${apikey['key']}`;
-                    const resultsVideos = await fetch(crawlVideosUrl, {
-                        method: "GET",
-                        mode: 'cors',
-                        headers: { "Content-Type": "Access-Control-Allow-Origin; text/plain" }
-                    }).catch((err) => {
-                        console.log(err);
-                    });
-                    const resultsVideosJson = await resultsVideos.json();
-
-                    const { items } = resultsVideosJson;
-                    items.forEach(item => {
-                        const { publishedAt, title, channelTitle } = item.snippet;
-                        const { videoId } = item.id;
-                        const imageurl = item.snippet.thumbnails.medium.url;
-
-                        const event = {
-                            title,
-                            start: publishedAt.split("T")[0],
-                            url: `https://www.youtube.com/watch?v=${videoId}`,
-                            imageurl,
-                            channelTitle
-                        };
-                        calendar.addEvent(event);
-                    });
-
-                    // nextPageTokenを取得
-                    const { nextPageToken } = resultsVideosJson;
-                    pagingToken = nextPageToken;
+                    await crawlVideos(calendar, pagingToken);
                 }
             },
             pagingButton: {
                 text: 'ページング',
                 click: async () => {
-                    const channelId = document.getElementById('dropdown-channels').value;
-
-                    // 動画情報を取得
-                    const crawlVideosUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=10&pageToken=${pagingToken}&key=${apikey['key']}`;
-                    const resultsVideos = await fetch(crawlVideosUrl, {
-                        method: "GET",
-                        mode: 'cors',
-                        headers: { "Content-Type": "Access-Control-Allow-Origin; text/plain" }
-                    }).catch((err) => {
-                        console.log(err);
-                    });
-                    const resultsVideosJson = await resultsVideos.json();
-
-                    const { items } = resultsVideosJson;
-                    items.forEach(item => {
-                        const { publishedAt, title, channelTitle } = item.snippet;
-                        const { videoId } = item.id;
-                        const imageurl = item.snippet.thumbnails.medium.url;
-
-                        const event = {
-                            title,
-                            start: publishedAt.split("T")[0],
-                            url: `https://www.youtube.com/watch?v=${videoId}`,
-                            imageurl,
-                            channelTitle
-                        };
-                        calendar.addEvent(event);
-                    });
-
-                    // nextPageTokenを取得
-                    const { nextPageToken } = resultsVideosJson;
-                    pagingToken = nextPageToken;
+                    await crawlVideos(calendar, pagingToken);
                 }
             }
         },
@@ -106,16 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     `<img src='${info.event.extendedProps.imageurl}' width='170' height='100'>`;
             }
         },
-        eventColor: 'white',
-        // テストイベント
-        // events: [
-        //     {
-        //         title: 'ijima',
-        //         start: '2019-08-06'
-        //     }, {
-        //         title: 'ijima2',
-        //         start: '2019-08-08'
-        //     }]
+        eventColor: 'white'
     })
     calendar.render();
 });

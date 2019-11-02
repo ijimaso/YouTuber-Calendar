70b4dd6471b39c11d5f4b420654ac70bb0d307ea
ijimaso <soxup0708@gmail.com>
2019-11-02 16:01:59 +0900

    delete

--- src/util/sleep.js
+++
@@ -1,8 +0,0 @@
-function sleep(a) {
-    const dt1 = new Date().getTime(); // 現在時刻のタイムスタンプ(ミリ秒)
-    let dt2 = new Date().getTime();
-    while (dt2 < dt1 + a) {
-        dt2 = new Date().getTime();
-    }
-    return;
-};
